718b5b94a9f553dcdd864f278b270802f7180c92
Brecht Verhoeve <46007524+bverhoeve@users.noreply.github.com>
2020-05-14 22:07:01 +0200

    Added a table

--- wolk/routes.py
+++ wolk/routes.py
@@ -4,7 +4,35 @@ from wolk import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    servers = [
+        {
+            'cloud_provider': 'AWS',
+            'name': 'T2 micro',
+            'api_name': 't2.micro',
+            'memory': '2 GiB',
+            'vCPUs': '2',
+            'GPUs': '0',
+            'GPU_model': 'None',
+            'storage': 'EBS Only',
+            'network_performance': '25 Mbps',
+            'linux_on_demand_cost': '$5 hourly',
+            'windows_on_demand_cost': '$20 hourly',
+        },
+        {
+            'cloud_provider': 'AWS',
+            'name': 'T2 micro',
+            'api_name': 't2.micro',
+            'memory': '2 GiB',
+            'vCPUs': '2',
+            'GPUs': '0',
+            'GPU_model': 'None',
+            'storage': 'EBS Only',
+            'network_performance': '25 Mbps',
+            'linux_on_demand_cost': '$5 hourly',
+            'windows_on_demand_cost': '$20 hourly',
+        }
+    ]
+    return render_template('index.html', servers=servers)
 
 @app.route('/aws')
 def aws():
