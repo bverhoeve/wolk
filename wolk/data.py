8b3758d798c86a9391e6adad1e092064ca2ab68b
Brecht Verhoeve <46007524+bverhoeve@users.noreply.github.com>
2020-05-31 15:11:39 +0200

    Revert "Fixed the S3 issue yay!"
    This reverts commit cd265d10ab07f2e5d32b677669cfd2123bb0d230.

--- wolk/data.py
+++ wolk/data.py
@@ -8,51 +8,58 @@ from .constants import (
     S3_BUCKET,
     AWS_DIR,
     AWS_EC2_FILE,
-    AWS_RDS_FILE,
-    AZURE_DIR,
-    GCP_DIR
+    AWS_RDS_FILE
 )
 
-# Initialisation of data directory
-if not os.path.exists(DATA_DIR):
-    logging.debug('No data directories yet, creating')
-    aws_path: str = os.path.join(DATA_DIR, AWS_DIR)
-    azure_path: str = os.path.join(DATA_DIR, AZURE_DIR)
-    gcp_path: str = os.path.join(DATA_DIR, GCP_DIR)
-    os.makedirs(aws_path, exist_ok=True)
-    os.makedirs(azure_path, exist_ok=True)
-    os.makedirs(gcp_path, exist_ok=True)
+def has_local_data(data_location: str = None) -> bool:
 
-def has_local_data(file_path: str = None) -> bool:
-    return os.path.exists(file_path) and os.path.isfile(file_path)
+    if os.path.exists(DATA_DIR) and os.path.isdir(DATA_DIR):
+
+        # If no data is specified, the general data directory is checked
+        if data_location is None:
+            return True
+        else:
+            data_dir = os.path.join(DATA_DIR, data_location)
+            return os.path.exists(data_dir) and os.path.isdir(data_dir) and not (len(os.listdir(data_dir)) == 0)
+    
+    return False
             
 def get_aws_data(data_type: str = None):
 
-    file_path: str = None
-    object_key: str = None
+    ec2_file = os.path.join(DATA_DIR, AWS_DIR, AWS_EC2_FILE)
+    rds_file = os.path.join(DATA_DIR, AWS_DIR, AWS_RDS_FILE)
 
-    if data_type is None or data_type == 'ec2':
-        file_path: str = os.path.join(DATA_DIR, AWS_DIR, AWS_EC2_FILE)
-        object_key: str = DATA_DIR + '/' + AWS_DIR + '/' + AWS_EC2_FILE
-    elif data_type == 'rds':
-        file_path: str = os.path.join(DATA_DIR, AWS_DIR, AWS_RDS_FILE)
-        object_key: str = DATA_DIR + '/' + AWS_DIR + '/' + AWS_RDS_FILE
-    else:
-        raise ValueError(f'{data_type} is not supported, choose "ec2" or "rds"')
+    ec2_s3_key = DATA_DIR + '/' + AWS_DIR + '/' + AWS_EC2_FILE
+    rds_s3_key = DATA_DIR + '/' + AWS_DIR + '/' + AWS_RDS_FILE
 
-    logging.debug(f'Loading AWS {data_type} data')
+    # there might be a bug here, since has_local_data only checks if the directory is not empty,
+    # not the actual file. Will probably fix later.
+    if has_local_data(AWS_DIR):
 
-    if has_local_data(file_path):
         logging.info('Locally cached data found')
-        with open(file_path, 'r') as fp:
-            return json.load(fp)
         
+        # Return EC2 data by default
+        if data_type is None or data_type == 'ec2':
+            with open(ec2_file, 'r') as fp:
+                return json.load(fp)
+
+        elif data_type == 'rds':
+            with open(rds_file, 'r') as fp:
+                return json.load(rds_file)
+        else:
+            raise ValueError(f'{data_type} is not supported, choose "ec2" or "rds"')
+
     else:
         # If no local data is present, download it from S3 and persist it.
         logging.info('No locally cached data found, downloading from S3')
         s3 = boto3.client('s3')
-        logging.debug(f'Downloading {object_key} from {S3_BUCKET} to {file_path}')
-        s3.download_file(S3_BUCKET, object_key, file_path)
-        
-        # Once downloaded, load in the cached data
-        return get_aws_data(data_type)
+        if data_type is None or data_type == 'ec2':
+            os.makedirs(os.path.dirname(os.path.join(DATA_DIR, AWS_DIR)), exist_ok=True)
+            s3.download_file(S3_BUCKET, ec2_s3_key, ec2_file)
+            return get_aws_data(data_type)
+        elif data_type == 'rds':
+            os.makedirs(os.path.dirname(os.path.join(DATA_DIR, AWS_DIR)), exist_ok=True)
+            s3.download_fileobj(S3_BUCKET, rds_s3_key, ec2_file)
+            return get_aws_data(data_type)
+        else:
+            raise ValueError(f'{data_type} is not supported, choose "ec2" or "rds"')
